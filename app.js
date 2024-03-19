89425c72fd39d054e381b833d494c5dbfc0d5874
gclee <raitmid56@gmail.com>
2024-03-19 17:58:54 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Hello from this NodeJS app!!!!!!</h1>
+    <p>This is new!</p>
+    <p>version 3</p>
+    <p>Try sending a request to /error and see what happens</p>
+    <script>
+      console.log(process.env);
+    </script>
+  `);
+});
+
+app.get('/error', (req, res) => {
+  process.exit(1);
+});
+
+app.listen(8080);
